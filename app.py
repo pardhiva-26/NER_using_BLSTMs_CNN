c22696cec9071b8f9f45487b2c18e27c286eb57d
Mandavalli Pardhiva Sri Sai <22cs01003@iitbbs.ac.in>
2025-06-27 11:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from ner_parser import Parser
 from flask_cors import CORS
+import nltk
+nltk.data.path.append("nltk_data") 
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for frontend-backend communication
